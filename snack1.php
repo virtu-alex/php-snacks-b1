427d65367209c4317c8fcbd0985091b2df2d0bb4
virtu-alex <alessio.cordari@gmail.com>
2022-08-30 17:35:03 +0200

    snack4

--- snack1.php
+++ snack1.php
@@ -1,6 +1,6 @@
 SNACK 1
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60 
+Olimpia Milano - Cantù | 55-60
 
 
 <?php
@@ -11,9 +11,8 @@ $teams = [
 
 $team_guest = [
     $guest_name = [" Chicago Inside", " Cleveland Cadavers", " NBArazzanti"],
-    $score_guest = ["70","68","61"]
+    $score_guest = ["70", "68", "61"]
 ]
-
 ?>
 
 <!DOCTYPE html>
@@ -23,14 +22,19 @@ $team_guest = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack 1</title>
 </head>
 
 <body>
     <div>
         <ul>
             <?php for ($i = 0; $i <= count($teams); $i++) : ?>
-                <li><span><?= $team_home[$i] ?> - </span></span><?= $guest_name[$i] ?><span> | <span> <?= $score_home[$i] ?></span> - <span> <?= $score_guest[$i] ?></span></li>
+                <li>
+                    <span><?= $team_home[$i] ?> - </span>
+                    <span><?= $guest_name[$i] ?></span>
+                    | <span> <?= $score_home[$i] ?></span>
+                    - <span> <?= $score_guest[$i] ?></span>
+                </li>
             <?php endfor; ?>
 
         </ul>
--- snack2.php
+++ snack2.php
@@ -17,9 +17,6 @@ if ((strlen($name) > 3) && (strpos($mail, '@') && strpos($mail, '.')) && (is_num
 } else {
     echo '<h1>Accesso negato</h1>';
 }
-
-
-
 ?>
 
 
@@ -34,7 +31,7 @@ if ((strlen($name) > 3) && (strpos($mail, '@') && strpos($mail, '.')) && (is_num
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack 2</title>
 </head>
 
 <body>
